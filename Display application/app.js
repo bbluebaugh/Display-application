6b79b89da3ebd6728b82fad9f78b9035bc5f878f
Brian <bbluebaugh@yahoo.com>
2019-10-04 22:48:59 -0400

    testing commit through atom

--- Display application/app.js
+++ Display application/app.js
@@ -1,7 +1,7 @@
 //@author bbluebaugh  @version 0.0.1
 //This will be the start of a javascript application that hopefully will be a cool
 //visualization of something
-
+//if I make a change then I should be able to commit it to github
 
 function setup(){
   createCanvas(400, 400);
