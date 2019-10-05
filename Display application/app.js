ed99f681839c6c7f7bd675a93e3c7197ca021368
Brian Bluebaugh <bbluebaugh@yahoo.com>
2019-10-04 22:30:28 -0400

    init commit
    attempting initial commit through github to try to sync with Atom.

---
+++ Display application/app.js
@@ -0,0 +1,14 @@
+//@author bbluebaugh  @version 0.0.1
+//This will be the start of a javascript application that hopefully will be a cool
+//visualization of something
+
+
+function setup(){
+  createCanvas(400, 400);
+
+}
+
+function draw(){
+  background(51);
+
+}
